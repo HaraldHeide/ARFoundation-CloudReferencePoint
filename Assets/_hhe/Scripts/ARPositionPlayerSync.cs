0d63f4ab2f258d0cb82037419b0df51f59a1c515
Harald Heide Gundersen <harald.heide.gundersen@hotmail.com>
2019-11-24 12:59:09 +0100

    sync script

---
+++ Assets/_hhe/Scripts/ARPositionPlayerSync.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class ARPositionPlayerSync : MonoBehaviour, IPunObservable
+{
+
+    private PhotonView photonView;
+    private Transform myCamera;
+    private Transform otherCamera;
+
+    TextMeshPro textMesh;
+
+    string myName = "";
+
+    private void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+        textMesh = GetComponentInChildren<TextMeshPro>();
+    }
+
+
+    void Start()
+    {
+        if(photonView.IsMine)
+        {
+            myCamera = Camera.main.transform;
+            myName = PhotonNetwork.LocalPlayer.NickName;
+        }
+        else
+        {
+            otherCamera = Camera.main.transform;
+        }
+        textMesh.text = myName;
+    }
+
+    void Update()
+    {
+        transform.position = Camera.main.transform.position;
+        transform.LookAt(otherCamera);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+       if(stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else if(stream.IsReading)
+       {
+            transform.position = (Vector3)stream.ReceiveNext();
+            transform.rotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
